5f1d1b54a622ba94c113d0ae4f98dcb8c57a25ee
alchen1218 <alchen@vmware.com>
2020-09-10 17:20:11 -0700

    testing

---
+++ build_and_deploy.sh
@@ -0,0 +1,18 @@
+set -e
+
+mvn package
+mvn --version
+terraform --version
+terragrunt --version
+
+pushd ../HELLO-WORLD-1/TESTING
+  python HelloWorld.py
+  python3 HelloWorld.py
+popd
+
+# This script assumes that the terraform repo is a sibling to this repo
+# pushd ../terraform/asana-jira-integration-lambda
+#   terragrunt init
+#   ./plan.sh $PATH_TO_JAR
+#   ./apply.sh $PATH_TO_JAR
+# popd
